1fdbb2ace8c8926a7a0b6b7958d33203afcd63a7
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-17 22:39:36 -0400

    add missing reverse aliases

--- aliases.php
+++ aliases.php
@@ -8,7 +8,9 @@ use Winter\Storm\Support\ClassLoader;
  */
 $aliases = [
     // Reverse alias to fix issue on PHP 7.2, see https://github.com/wintercms/wn-user-plugin/runs/2122181184
+    RainLab\Pages\Classes\MenuItem::class                           => Winter\Pages\Classes\MenuItem::class,
     RainLab\Pages\Classes\Page::class                               => Winter\Pages\Classes\Page::class,
+    RainLab\Pages\Classes\SnippetManager::class                     => Winter\Pages\Classes\SnippetManager::class,
 
     // Regular aliases
     Winter\Translate\Plugin::class                                  => RainLab\Translate\Plugin::class,
