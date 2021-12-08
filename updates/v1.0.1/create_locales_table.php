5e403c25fe5141c13a5e381cd6ce09aad0bcc116
Mike Robinson <multiwebinc@users.noreply.github.com>
2021-12-08 11:22:57 -0600

    Update create_locales_table.php

--- updates/v1.0.1/create_locales_table.php
+++ updates/v1.0.1/create_locales_table.php
@@ -8,15 +8,17 @@ class CreateLocalesTable extends Migration
 
     public function up()
     {
-        Schema::create('rainlab_translate_locales', function($table)
-        {
-            $table->engine = 'InnoDB';
-            $table->increments('id');
-            $table->string('code')->index();
-            $table->string('name')->index()->nullable();
-            $table->boolean('is_default')->default(0);
-            $table->boolean('is_enabled')->default(0);
-        });
+        if (!Schema::hasTable('rainlab_translate_locales')) {
+            Schema::create('rainlab_translate_locales', function($table)
+            {
+                $table->engine = 'InnoDB';
+                $table->increments('id');
+                $table->string('code')->index();
+                $table->string('name')->index()->nullable();
+                $table->boolean('is_default')->default(0);
+                $table->boolean('is_enabled')->default(0);
+            });
+        }
     }
 
     public function down()
