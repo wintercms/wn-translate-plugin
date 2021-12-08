0366150e0beaeedafaf4d7e104f69728eb429943
Mike Robinson <multiwebinc@users.noreply.github.com>
2021-12-08 11:23:24 -0600

    Update create_messages_table.php

--- updates/v1.0.1/create_messages_table.php
+++ updates/v1.0.1/create_messages_table.php
@@ -8,13 +8,15 @@ class CreateMessagesTable extends Migration
 
     public function up()
     {
-        Schema::create('rainlab_translate_messages', function($table)
-        {
-            $table->engine = 'InnoDB';
-            $table->increments('id');
-            $table->string('code')->index()->nullable();
-            $table->mediumText('message_data')->nullable();
-        });
+        if (!Schema::hasTable('rainlab_translate_messages')) {
+            Schema::create('rainlab_translate_messages', function($table)
+            {
+                $table->engine = 'InnoDB';
+                $table->increments('id');
+                $table->string('code')->index()->nullable();
+                $table->mediumText('message_data')->nullable();
+            });
+        }
     }
 
     public function down()
