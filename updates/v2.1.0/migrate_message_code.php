3a9d2320abede2bbf769b7f4edb8b130e72704fa
Marc Jauvin <marc.jauvin@gmail.com>
2022-02-23 20:28:53 -0500

    Generate message code using md5() (#2)
    generate message codes using md5()

---
+++ updates/v2.1.0/migrate_message_code.php
@@ -0,0 +1,49 @@
+<?php namespace Winter\Translate\Updates;
+
+use Schema;
+use Str;
+use Winter\Storm\Database\Updates\Migration;
+use Winter\Translate\Models\Message;
+
+class MigrateMessageCode extends Migration
+{
+    const TABLE_NAME = 'winter_translate_messages';
+
+    public function up()
+    {
+        foreach (Message::all() as $message) {
+            $default_message = $message->message_data['x'];
+            $message->code = Message::makeMessageCode($default_message);
+            $message->save();
+        }
+    }
+
+    public function down()
+    {
+        if (!Schema::hasTable(self::TABLE_NAME)) {
+            return;
+        }
+
+        foreach (Message::all() as $message) {
+            $default_message = $message->message_data['x'];
+            $message->code = static::makeLegacyMessageCode($default_message);
+            $message->save();
+        }
+    }
+
+    public static function makeLegacyMessageCode($messageId)
+    {
+        $separator = '.';
+
+        // Convert all dashes/underscores into separator
+        $messageId = preg_replace('!['.preg_quote('_').'|'.preg_quote('-').']+!u', $separator, $messageId);
+
+        // Remove all characters that are not the separator, letters, numbers, or whitespace.
+        $messageId = preg_replace('![^'.preg_quote($separator).'\pL\pN\s]+!u', '', mb_strtolower($messageId));
+
+        // Replace all separator characters and whitespace by a single separator
+        $messageId = preg_replace('!['.preg_quote($separator).'\s]+!u', $separator, $messageId);
+
+        return Str::limit(trim($messageId, $separator), 250);
+    }
+}
