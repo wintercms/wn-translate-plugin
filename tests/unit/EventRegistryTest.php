4dcaf1c019d1179258a9646d90e638606019c98b
Marc Jauvin <marc.jauvin@gmail.com>
2022-08-15 09:38:59 -0400

    add unit test to verify fields added dynamically in tabs get localized

---
+++ tests/unit/EventRegistryTest.php
@@ -0,0 +1,60 @@
+<?php namespace Winter\Translate\Tests\Unit;
+
+use Event;
+use Backend\Widgets\Form;
+use Winter\Storm\Database\Model;
+
+class EventRegistryTest extends \Winter\Translate\Tests\TranslatePluginTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testRegisterModelTranslation()
+    {
+        FormTestModel::extend(function ($model) {
+            $model->translatable = array_merge($model->translatable, ['testField', 'tabTestField', 'secondaryTabTestField']);
+        });
+
+        Event::listen('backend.form.extendFieldsBefore', function($widget) {
+            $widget->tabs['fields']['tabTestField'] = [
+                'label'   => 'Tab Test Field',
+                'type'    => 'text',
+                'tab'     => 'New Tab',
+            ];
+            $widget->secondaryTabs['fields']['secondaryTabTestField'] = [
+                'label'   => 'Secondary Tab Test Field',
+                'type'    => 'text',
+                'tab'     => 'Another Tab',
+            ];
+        });
+
+        $controller = new \Backend\Classes\Controller();
+        $form = new Form($controller, [
+            'model' => new FormTestModel,
+            'arrayName' => 'array',
+            'fields' => [
+                'testField' => [
+                    'type' => 'text',
+                    'label' => 'Test 1'
+                ],
+            ]
+        ]);
+
+        $form->bindToController();
+        $this->assertInstanceOf(Form::class, $form);
+        $this->assertEquals('mltext', $form->fields['testField']['type']);
+        $this->assertEquals('mltext', $form->tabs['fields']['tabTestField']['type']);
+        $this->assertEquals('mltext', $form->secondaryTabs['fields']['secondaryTabTestField']['type']);
+    }
+}
+
+class FormTestModel extends Model
+{
+    public $implement = [
+        'Winter.Translate.Behaviors.TranslatableModel',
+    ];
+
+    public $translatable = [];
+}
