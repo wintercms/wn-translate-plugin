64081efe9831b8a9ee19ee5868258121deb4e9c4
AIC BV <89913092+AIC-BV@users.noreply.github.com>
2023-10-19 10:19:32 +0200

    Create mltextarea.js

---
+++ formwidgets/mltextarea/assets/js/mltextarea.js
@@ -0,0 +1,24 @@
+function updateTextareaLayout($element) {
+
+    // get elements
+    const $parent = $element.parentElement
+    const $btn = $parent.querySelector('.ml-btn[data-active-locale]')
+    const $dropdown = $parent.querySelector('.ml-dropdown-menu[data-locale-dropdown]')
+
+    // set ML button position
+    const elementHeight = $element.offsetHeight
+    const scrollHeight = $element.scrollHeight
+    const showScrollbar = (scrollHeight - elementHeight) > 0 ? true : false
+
+    if (showScrollbar) {
+        const scrollbarWidth = $element.offsetWidth - $element.clientWidth
+        $element.style.paddingRight = `${scrollbarWidth + 23}px`
+        $btn.style.right = `${scrollbarWidth -1}px`
+        $dropdown.style.right = `${scrollbarWidth -2}px`
+    } else {
+        $element.style.paddingRight = ''
+        $btn.style.right = ''
+        $dropdown.style.right = ''
+    }
+
+}
