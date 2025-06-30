c8af6f141c18fc621d3c9c6fc79ed9cfab6e987d
Isaiah Paget <isaiahpaget@gmail.com>
2025-06-30 12:26:56 -0700

    media finder working

--- formwidgets/mlmediafinder/assets/js/mlmediafinder.js
+++ formwidgets/mlmediafinder/assets/js/mlmediafinder.js
@@ -35,6 +35,7 @@
 
     MLMediaFinder.DEFAULTS = {
         placeholderField: null,
+        copyHandler: null,
         defaultLocale: 'en',
         mediaPath: '/',
     }
@@ -43,6 +44,7 @@
 
         this.$el.multiLingual()
         this.$el.on('setLocale.oc.multilingual', this.proxy(this.onSetLocale))
+        this.$el.on('copyLocale.oc.multilingual', this.proxy(this.onCopyLocale))
         this.$el.one('dispose-control', this.proxy(this.dispose))
         // Listen for change event from mediafinder
         this.$findValue.on('change', this.proxy(this.setValue))
@@ -59,6 +61,7 @@
 
     MLMediaFinder.prototype.dispose = function() {
         this.$el.off('setLocale.oc.multilingual', this.proxy(this.onSetLocale))
+        this.$el.off('copyLocale.oc.multilingual', this.proxy(this.onCopyLocale))
         this.$el.off('dispose-control', this.proxy(this.dispose))
         this.$findValue.off('change', this.proxy(this.setValue))
 
@@ -77,6 +80,10 @@
 
 
     MLMediaFinder.prototype.onSetLocale = function(e, locale, localeValue) {
+        this.setPath(localeValue)
+    }
+
+    MLMediaFinder.prototype.onCopyLocale = function(e, locale, localeValue) {
         this.setPath(localeValue)
     }
 
